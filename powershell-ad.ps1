0ee42bd193396f1f5d78d4a0b6c2ffcc170b6de9
mechanicus01 <mechanicus01@gmail.com>
2022-12-09 22:09:42 -0600

    Update powershell-ad.ps1

--- powershell-ad.ps1
+++ powershell-ad.ps1
@@ -9,3 +9,11 @@ for ($i=0; $i -lt 10; $i++) {
 
     New-ADUser -Name $username -SamAccountName $username -UserPrincipalName $email -EmailAddress $email -Path "OU=mydomain,DC=iw,DC=inc" -AccountPassword (ConvertTo-SecureString -AsPlainText "1password2?" -Force) -Enabled $true -ChangePasswordAtLogon $true
 }
+
+
+# Create an OU in Active Directory
+new-adorganizationalunit -name "mydomain2" -path "DC=iw,DC=inc"
+
+# Create one user 
+New-ADUser -Name "John D" -GivenName "John" -Surname "Doe" -SamAccountName "jdoe" -UserPrincipalName "jdoe@iwdevops.net" -EmailAddress "jdoe@iwdevops.net" -Path "OU=mydomain,DC=iw,DC=inc" -AccountPassword (ConvertTo-SecureString -AsPlainText "password" -Force) -Enabled $true -ChangePasswordAtLogon $false
+
