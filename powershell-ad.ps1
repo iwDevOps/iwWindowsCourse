35e7983e450c8336e8631797f130feed27f388c0
mechanicus01 <mechanicus01@gmail.com>
2022-12-16 22:39:23 -0600

    Update powershell-ad.ps1

--- powershell-ad.ps1
+++ powershell-ad.ps1
@@ -10,7 +10,7 @@ $alphabet = "ABCDEFGHIJ"
 
 for ($i=0; $i -lt 10; $i++) {
     $letter = $alphabet[$i]
-    $username = $letter+$i + "_user" 
+    $username = $letter + "_user" 
     $email = $username + "@mydomain.com"
 
     New-ADUser -Name $username -SamAccountName $username -UserPrincipalName $email -EmailAddress $email -Path "OU=mydomain,DC=iw,DC=inc" -AccountPassword (ConvertTo-SecureString -AsPlainText "1password2?" -Force) -Enabled $true -ChangePasswordAtLogon $true
